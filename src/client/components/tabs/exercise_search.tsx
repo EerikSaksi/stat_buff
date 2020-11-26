ce7bfee7b00fae58c92511ff6da4e2ae914dfbca
Eerik Saksi <saksi.eerik@gmail.com>
2020-11-26 16:04:40 +0200

    Crea

--- src/client/components/tabs/exercise_search.tsx
+++ src/client/components/tabs/exercise_search.tsx
@@ -1,62 +1,50 @@
-import React, {useEffect, useState} from 'react'
-import {useQuery, gql} from '@apollo/client'
-import {View, Text, FlatList, TextInput} from 'react-native'
-import Loading from '../../util_components/loading'
-import {unslugify, slugify} from '../../util_components/slug'
+import React, {useEffect, useState} from 'react';
+import {useQuery, gql} from '@apollo/client';
+import {Text, FlatList} from 'react-native';
+import Loading from '../../util_components/loading';
+import {slugify} from '../../util_components/slug';
 const EXERCISE_SEARCH = gql`query($input: String!){
   exercises(filter: {slugName: {includesInsensitive: $input}}, orderBy: POPULARITY_RANKING_ASC, first: 5){
     nodes{
       slugName
     }
   }
-}`
-
-const ExerciseSearch: React.FC<{input: string}> = ({input}) => {
-  const [sluggedInput, setSluggedInput] = useState("")
+}`;
+const ExerciseSearch: React.FC<{input: string;}> = ({input}) => {
+  const [sluggedInput, setSluggedInput] = useState("");
   useEffect(() => {
-    setSluggedInput(slugify(input))
-  }, [input])
-  const {data, loading} = useQuery(EXERCISE_SEARCH, {
+    setSluggedInput(slugify(input));
+  }, [input]);
+  const {data} = useQuery(EXERCISE_SEARCH, {
     variables: {input: sluggedInput},
     skip: input === ""
-  })
+  });
 
-  if (!data) {
-    return (<Loading />)
+  if (!data && input !== "") {
+    return (<Loading />);
   }
 
   //either example search or actual search
   const exercises = input === ""
     ? ["bench-press", "deadlift", "squat", "shoulder-press", "pull-ups"]
-    : data.exercises.nodes.map(exercise => exercise.slugName)
+    : data.exercises.nodes.map(exercise => exercise.slugName);
 
   return (
     <React.Fragment>
-      {
-        input === ""
-          ?
-          <Text style={{fontSize: 20, }}>
-            Most popular searches
+      {input === ""
+        ?
+        <Text style={{fontSize: 20, }}>
+          Most popular searches
           </Text>
-          : undefined
-      }
+        : undefined}
       <FlatList data={exercises} style={{width: '100%'}}
-        renderItem={({item}) =>
-          <View key={item} style={{flex: 1, alignItems: 'center', justifyContent: 'center', }}>
-            <View style={{flex: 1, flexDirection: 'row', }}>
-              <View style={{flex: 1}}>
-                <Text>
-                  {unslugify(item)}
-                </Text>
-              </View>
-              <TextInput style={{flex: 1, textAlign: 'center'}} placeholder={'Weight (kg)'} />
-              <TextInput style={{flex: 1, textAlign: 'center'}} placeholder={'Reps'} />
-            </View>
-          </View>
-        }
+        renderItem={({item}) => {
+          return <ExerciseSearchResult item />;
+        }}
       >
       </FlatList>
     </React.Fragment>
-  )
-}
-export default ExerciseSearch
+  );
+};
+export default ExerciseSearch;
+
