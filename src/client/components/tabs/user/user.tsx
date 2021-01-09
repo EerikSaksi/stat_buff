5f25024ebf0595396f327df8c240ed36c63a8431
Eerik Saksi <saksi.eerik@gmail.com>
2021-01-09 18:27:27 +0200

    Stylesheet on user

--- src/client/components/tabs/user/user.tsx
+++ src/client/components/tabs/user/user.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { ApolloProvider, gql, useApolloClient, useLazyQuery, useQuery } from "@apollo/client";
-import { Text, View, StatusBar } from "react-native";
+import { gql, useLazyQuery, useQuery } from "@apollo/client";
+import { Text, View, StatusBar, StyleSheet } from "react-native";
 import Loading from "../../../util_components/loading";
 import ExerciseModal from "./exercise_modal";
 import SpriteSelector from "../../../sprites/sprite_selector";
@@ -36,83 +36,95 @@ const STRENGTH = gql`
   }
 `;
 
+const styles = StyleSheet.create({
+  root: {
+    justifyContent: "center",
+    flex: 10,
+    alignItems: "center",
+    top: StatusBar.currentHeight,
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "100%",
+  },
+  updateBodyStats: {
+    flex: 1,
+    zIndex: 100,
+  },
+  flexOne: {
+    flex: 1,
+  },
+  welcomeBack: {
+    fontSize: 30,
+    textAlign: "center",
+  },
+  dph: {
+    textAlign: "center",
+    fontSize: 20,
+  },
+  explanation: {
+    textAlign: "center",
+    fontSize: 12,
+    overflow: "scroll",
+  },
+  sprite: {
+    flex: 5,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    zIndex: -1,
+  },
+  trackWorkout: {
+    flex: 1,
+    width: "30%",
+  },
+});
 type NavigationProps = { params: { username: string } };
 const User: React.FC<{ route: NavigationProps }> = ({ route }) => {
-  const client = useApolloClient()
   const { username } = route.params;
   const [strengthModalVisible, setStrengthModalVisible] = useState(false);
   const [bodystatsModalVisible, setBodystatsModalVisible] = useState(false);
   const [workoutModalVisible, setWorkoutModalVisible] = useState(false);
   const { data } = useQuery(USER, {
     variables: { username },
-    client
   });
-
-  const [fetchStrength, { data: exerciseData, loading }] = useLazyQuery(STRENGTH, {
-    client
-  });
+  const [fetchStrength, { data: exerciseData, loading }] = useLazyQuery(STRENGTH);
   const [fetchBodyStats, { data: userBodyStats }] = useLazyQuery(USER_BODY_STATS, {
-    variables: { username },
-    onCompleted: (data) => {
-      //haven't input their body stats, then open the option
-      if (!data.bodystat) {
-        setBodystatsModalVisible(true);
-      }
-    },
-    client
+    variables: {username}
   });
   useEffect(() => {
     fetchBodyStats();
     fetchStrength();
   }, []);
-  //const { skillTitle } = useSkillTitle(exerciseData && exerciseData.calculateStrengthStats ? exerciseData.calculateStrengthStats.dph : undefined);
-  const skillTitle = "advanced";
+  const { skillTitle } = useSkillTitle(exerciseData && exerciseData.calculateStrengthStats ? exerciseData.calculateStrengthStats.dph : undefined);
   if (!data) {
     return <Loading />;
   }
   return (
-    <ApolloProvider client = {client}>
-      <View
-        style={{
-          justifyContent: "center",
-          flex: 10,
-          alignItems: "center",
-          top: StatusBar.currentHeight,
-        }}
-      >
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            width: "100%",
-          }}
-        >
-          <Button style={{ flex: 1, zIndex: 100 }} title="Update Body Stats" onPress={() => setBodystatsModalVisible(true)} />
-          <Button style={{ flex: 1 }} disabled={!(userBodyStats && userBodyStats.bodystat)} title="Update Lifts" onPress={() => setStrengthModalVisible(true)} />
-        </View>
-        <View style={{ flex: 1 }}>
-          <Text style={{ fontSize: 30, textAlign: "center" }}> {`Welcome back, ${username}.`} </Text>
-          {exerciseData && exerciseData.calculateStrengthStats ? (
-            <View>
-              <Text style={{ textAlign: "center", fontSize: 20 }}>{`Your character has DPH: ${exerciseData.calculateStrengthStats.dph}`}</Text>
-              <Text style={{ textAlign: "center", fontSize: 12, overflow: "scroll" }}>{`Damage Per Hit = Stronger than ${exerciseData.calculateStrengthStats.averageStrength}% * ${
-                exerciseData.calculateStrengthStats.numExercises
-              } exercise${exerciseData.calculateStrengthStats.numExercises === 1 ? "" : "'s"} tracked`}</Text>
-            </View>
-          ) : null}
-        </View>
-        <ExerciseModal visible={strengthModalVisible} setVisible={setStrengthModalVisible} username={username} refetchParent={fetchStrength} />
-        <BodyStatsModal visible={bodystatsModalVisible} setVisible={setBodystatsModalVisible} username={username} refetchParent={fetchBodyStats} />
-        <WorkoutModal visible={workoutModalVisible} setVisible={setWorkoutModalVisible} username={username} skillTitle={skillTitle} />
-        <View style={{ flex: 5, justifyContent: "flex-end", alignItems: "center", zIndex: -1 }}>
-          {(exerciseData && exerciseData.averageStrength) || !loading ? <SpriteSelector spriteName={skillTitle} /> : <Loading />}
-        </View>
-        <View style={{ flex: 1, width: "30%" }}>
-          <Button disabled={!(exerciseData && exerciseData.calculateStrengthStats)} title="Log workout" onPress={() => setWorkoutModalVisible(true)} />
-        </View>
+    <View style={styles.root}>
+      <View style={styles.row}>
+        <Button style={styles.updateBodyStats} title="Update Body Stats" onPress={() => setBodystatsModalVisible(true)} />
+        <Button style={styles.flexOne} disabled={!(userBodyStats && userBodyStats.bodystat)} title="Update Lifts" onPress={() => setStrengthModalVisible(true)} />
       </View>
-    </ApolloProvider>
+      <View style={styles.flexOne}>
+        {exerciseData && exerciseData.calculateStrengthStats ? (
+          <View>
+            <Text style={styles.dph}>{`Your character has DPH: ${exerciseData.calculateStrengthStats.dph} (${skillTitle})`}</Text>
+            <Text style={styles.explanation}>{`Damage Per Hit = Stronger than ${exerciseData.calculateStrengthStats.averageStrength}% * ${exerciseData.calculateStrengthStats.numExercises} exercise${
+              exerciseData.calculateStrengthStats.numExercises === 1 ? "" : "'s"
+            } tracked`}</Text>
+          </View>
+        ) : null}
+      </View>
+      <ExerciseModal visible={strengthModalVisible} setVisible={setStrengthModalVisible} username={username} refetchParent={fetchStrength} />
+      <BodyStatsModal visible={bodystatsModalVisible} setVisible={setBodystatsModalVisible} username={username} refetchParent={fetchBodyStats} />
+      <WorkoutModal visible={workoutModalVisible} setVisible={setWorkoutModalVisible} username={username}         skillTitle={skillTitle} />
+      <View style={styles.sprite}>{(exerciseData && exerciseData.averageStrength) || !loading ? <SpriteSelector spriteName={skillTitle} /> : <Loading />}</View>
+      <View style={styles.trackWorkout}>
+        <Button disabled={!(exerciseData && exerciseData.calculateStrengthStats)} title="Log workout" onPress={() => setWorkoutModalVisible(true)} />
+      </View>
+    </View>
   );
 };
 export default User;
