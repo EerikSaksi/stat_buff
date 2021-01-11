044c919f98285cbb68df6ba9eff8e2b2a4b8c0fb
Eerik Saksi <saksi.eerik@gmail.com>
2021-01-11 12:10:41 +0200

    Switching to master

--- src/backend/src/google_auth.js
+++ src/backend/src/google_auth.js
@@ -1,14 +1,16 @@
 const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client(process.env.GOOGLE_AUTH_CLIENT);
-require('https').globalAgent.options.ca = require('ssl-root-cas').create();
+require("https").globalAgent.options.ca = require("ssl-root-cas").create();
 //validate the token that we are given and return the googleID that it was sent by
 async function tokenToGoogleID(headerAuth) {
   const ticket = await client.verifyIdToken({
     idToken: headerAuth.split(" ")[1],
-    audience: process.env.GOOGLE_AUTH_CLIENT, 
+    audience: process.env.GOOGLE_AUTH_CLIENT,
   });
   const payload = ticket.getPayload();
-  const userid = payload['sub'];
-  console.log(userid)
+  return {
+    id: payload["sub"],
+    email: payload["email"],
+  }
 }
 module.exports = tokenToGoogleID;
