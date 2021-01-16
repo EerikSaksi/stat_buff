e44c7b74fbaeee5b3d0d2d74e460c43ae9027d76
Eerik Saksi <saksi.eerik@gmail.com>
2021-01-16 18:09:04 +0200

    Testing

--- src/client/tests/check_boxes.test.tsx
+++ src/client/tests/check_boxes.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Checkboxes from '../components/check_boxes';
-describe('<Checkboxes />', () => {
-  it('has 1 child', async () => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    const tree = renderer.create(<Checkboxes setAllChecksFilled = {(arg: boolean) => {}}/>).toJSON();
-  })
+import React from "react";
+import renderer from "react-test-renderer";
+import Checkboxes from "../components/check_boxes";
+describe("<Checkboxes />", () => {
+  jest.mock("react-native-elements", () => {
+    return { CheckBox: <></> };
+  });
+  it("has 1 child", async () => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const tree = renderer.create(<Checkboxes setAllChecksFilled={(arg: boolean) => {}} />).toJSON();
+    expect(tree).toMatchSnapshot("");
+  });
 });
