ed6619ee7409312dfdf91ecfe161e3894aad64fb
Eerik Saksi <saksi.eerik@gmail.com>
2021-01-16 16:56:42 +0200

    Changing to requires

--- src/client/setupJest.js
+++
@@ -1,6 +0,0 @@
-jest.mock('@expo/vector-icons', () => {
-  const mockComponent = require('react-native/jest/mockComponent')
-  return {
-    CachedIcon: mockComponent('Icon')
-  }
-})
--- src/client/tests/check_boxes.test.tsx
+++ src/client/tests/check_boxes.test.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Checkboxes from '../components/check_boxes';
 describe('<Checkboxes />', () => {
-  it('has 1 child', () => {
+  it('has 1 child', async () => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
     const tree = renderer.create(<Checkboxes setAllChecksFilled = {(arg: boolean) => {}}/>).toJSON();
-  });
+  })
 });
