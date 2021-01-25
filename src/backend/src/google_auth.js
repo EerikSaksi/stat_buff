3179a3e1b7eb92011af77e140a22d9d2505fef9e
Eerik Saksi <saksi.eerik@gmail.com>
2021-01-25 22:07:55 +0200

    no audience

--- src/backend/src/google_auth.js
+++ src/backend/src/google_auth.js
@@ -1,12 +1,10 @@
 const { OAuth2Client } = require("google-auth-library");
-const client = new OAuth2Client(process.env.GOOGLE_AUTH_CLIENT);
+const client = new OAuth2Client(process.env.GOOGLE_AUTH_ANDROID_CLIENT);
 require("https").globalAgent.options.ca = require("ssl-root-cas").create();
 //validate the token that we are given and return the googleID that it was sent by
 async function tokenToGoogleID(headerAuth) {
-  console.log(process.env.GOOGLE_AUTH_ANDROID_CLIENT)
   const ticket = await client.verifyIdToken({
     idToken: headerAuth.split(" ")[1],
-    audience: [process.env.GOOGLE_AUTH_ANDROID_CLIENT]
   });
   const payload = ticket.getPayload();
   return {
