17c94b053d350b93dcdc5bf13565b244f20562a0
Eerik Saksi <saksi.eerik@gmail.com>
2021-01-26 18:56:16 +0200

    Removed auth client param

--- src/backend/src/google_auth.js
+++ src/backend/src/google_auth.js
@@ -2,7 +2,7 @@ const {OAuth2Client} = require('google-auth-library');
 
 //validate the token that we are given and return the googleID that it was sent by
 async function tokenToGoogleID(headerAuth){
-  const client = new OAuth2Client(process.env.GOOGLE_AUTH_CLIENT);
+  const client = new OAuth2Client();
   const ticket = await client.verifyIdToken({
     idToken: headerAuth.split(" ")[1],
     audience: [process.env.GOOGLE_AUTH_ANDROID_CLIENT, process.env.GOOGLE_AUTH_IOS_CLIENT]
