d7a9f4f644f2bb4b848dc9beaa847fd85b33c917
Eerik Saksi <saksi.eerik@gmail.com>
2021-01-26 19:11:03 +0200

    just ios id

--- src/backend/src/google_auth.js
+++ src/backend/src/google_auth.js
@@ -1,15 +1,14 @@
 const { OAuth2Client } = require("google-auth-library");
-const client = new OAuth2Client();
 const fetch = require('node-fetch')
 require("https").globalAgent.options.ca = require("ssl-root-cas").create();
 //validate the token that we are given and return the googleID that it was sent by
+const client = new OAuth2Client(process.env.GOOGLE_AUTH_IOS_CLIENT);
 async function tokenToGoogleID(headerAuth) {
   await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${headerAuth.split(" ")[1]}`)
   .then(response => response.text())
   .then(text => console.log(text))
   const ticket = await client.verifyIdToken({
     idToken: headerAuth.split(" ")[1],
-    audience: [process.env.GOOGLE_AUTH_ANDROID_CLIENT, process.env.GOOGLE_AUTH_IOS_CLIENT]
   });
   const payload = ticket.getPayload();
   console.log(payload)
