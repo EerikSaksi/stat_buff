b4868d3e8e29923cf5691388b0f6215efe25eab2
Eerik Saksi <saksi.eerik@gmail.com>
2021-01-26 19:51:51 +0200

    no params

--- src/backend/src/google_auth.js
+++ src/backend/src/google_auth.js
@@ -2,7 +2,7 @@ const { OAuth2Client } = require("google-auth-library");
 const fetch = require('node-fetch')
 require("https").globalAgent.options.ca = require("ssl-root-cas").create();
 //validate the token that we are given and return the googleID that it was sent by
-const client = new OAuth2Client(process.env.GOOGLE_AUTH_CLIENT, process.env.GOOGLE_AUTH_SECRET);
+const client = new OAuth2Client();
 async function tokenToGoogleID(headerAuth) {
   await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${headerAuth.split(" ")[1]}`)
   .then(response => response.text())
