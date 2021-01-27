87c6af0ceee8ebb116f894bd27175a550f089c94
Eerik Saksi <saksi.eerik@gmail.com>
2021-01-27 20:00:05 +0200

    SafeAreaView with height 100%

--- src/client/components/tabs/user/exercise_modal.tsx
+++ src/client/components/tabs/user/exercise_modal.tsx
@@ -1,46 +1,45 @@
 import React, { useEffect, useState } from "react";
-import { Text, Switch, View, Modal, TextInput, StyleSheet, StatusBar } from "react-native";
+import { Text, Switch, View, Modal, TextInput, StyleSheet, StatusBar, SafeAreaView } from "react-native";
 import ExerciseSearch from "./exercise_search";
 import { Ionicons } from "@expo/vector-icons";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import globalStyles from "../../../style/global";
 const styles = StyleSheet.create({
   paddingWrap: { paddingTop: "5%", flex: 1 },
   arrow: { color: "black", fontSize: 40, left: 0, position: "absolute" },
   text: { marginRight: "2%" },
   listContainer: { flex: 10, justifyContent: "center", alignItems: "center" },
+  safeArea: { height: "100%" },
 });
 const ExerciseModal: React.FC<{ visible: boolean; setVisible: (b: boolean) => void; username: string; refetchParent: () => void }> = ({ visible, setVisible, username, refetchParent }) => {
   const [exerciseInput, setExerciseInput] = useState("");
   const [onlyShowTracked, setOnlyShowTracked] = useState(false);
   const [onlyBodyweight, setOnlyBodyweight] = useState(false);
-
-  //hide the status bar when this modal is visible (overlaps on ios)
-  useEffect(() => {
-    StatusBar.setHidden(visible)
-  }, [visible])
   return (
     <Modal visible={visible} onDismiss={() => setVisible(false)} onRequestClose={() => setVisible(false)} animationType={"slide"}>
-      <Ionicons onPress={() => setVisible(false)} name="arrow-back-sharp" style={styles.arrow} />
-      <View style={styles.paddingWrap}>
-        <View style={globalStyles.row}>
+      <SafeAreaView style = { styles.safeArea }>
+        <Ionicons onPress={() => setVisible(false)} name="arrow-back-sharp" style={styles.arrow} />
+        <View style={styles.paddingWrap}>
           <View style={globalStyles.row}>
-            <View style={globalStyles.container}>
-              <Text style={styles.text}>Only tracked exercises</Text>
-              <Switch value={onlyShowTracked} onValueChange={(v) => setOnlyShowTracked(v)} />
-            </View>
-            <View style={globalStyles.container}>
-              <Text style={styles.text}>Only bodyweight</Text>
-              <Switch value={onlyBodyweight} onValueChange={(v) => setOnlyBodyweight(v)} />
+            <View style={globalStyles.row}>
+              <View style={globalStyles.container}>
+                <Text style={styles.text}>Only tracked exercises</Text>
+                <Switch value={onlyShowTracked} onValueChange={(v) => setOnlyShowTracked(v)} />
+              </View>
+              <View style={globalStyles.container}>
+                <Text style={styles.text}>Only bodyweight</Text>
+                <Switch value={onlyBodyweight} onValueChange={(v) => setOnlyBodyweight(v)} />
+              </View>
             </View>
           </View>
-        </View>
-        <View style={styles.listContainer}>
-          <View>
-            <TextInput value={exerciseInput} onChangeText={(t) => setExerciseInput(t)} placeholder="Search for exercises" />
+          <View style={styles.listContainer}>
+            <View>
+              <TextInput value={exerciseInput} onChangeText={(t) => setExerciseInput(t)} placeholder="Search for exercises" />
+            </View>
+            <ExerciseSearch refetchParent={refetchParent} input={exerciseInput} username={username} onlyShowTracked={onlyShowTracked} onlyBodyweight={onlyBodyweight} />
           </View>
-          <ExerciseSearch refetchParent={refetchParent} input={exerciseInput} username={username} onlyShowTracked={onlyShowTracked} onlyBodyweight = {onlyBodyweight}/>
         </View>
-      </View>
+      </SafeAreaView>
     </Modal>
   );
 };
