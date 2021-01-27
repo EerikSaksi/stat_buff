6c9dc86b44e55be85565a299f79c0c6fd9aefed5
Eerik Saksi <saksi.eerik@gmail.com>
2021-01-27 11:35:31 +0200

    Safeareaview is inside the modal

--- src/client/components/tabs/user/exercise_modal.tsx
+++ src/client/components/tabs/user/exercise_modal.tsx
@@ -3,7 +3,7 @@ import { Text, Switch, View, Modal, TextInput, StyleSheet } from "react-native";
 import ExerciseSearch from "./exercise_search";
 import { Ionicons } from "@expo/vector-icons";
 import globalStyles from "../../../style/global";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView } from "react-native-safe-area-context";
 const styles = StyleSheet.create({
   paddingWrap: { paddingTop: "5%", flex: 1 },
   arrow: { color: "black", fontSize: 40, left: 0, position: "absolute" },
@@ -15,31 +15,31 @@ const ExerciseModal: React.FC<{ visible: boolean; setVisible: (b: boolean) => vo
   const [onlyShowTracked, setOnlyShowTracked] = useState(false);
   const [onlyBodyweight, setOnlyBodyweight] = useState(false);
   return (
-    <SafeAreaView>
     <Modal visible={visible} onDismiss={() => setVisible(false)} onRequestClose={() => setVisible(false)} animationType={"slide"}>
-      <Ionicons onPress={() => setVisible(false)} name="arrow-back-sharp" style={styles.arrow} />
-      <View style={styles.paddingWrap}>
-        <View style={globalStyles.row}>
+      <SafeAreaView>
+        <Ionicons onPress={() => setVisible(false)} name="arrow-back-sharp" style={styles.arrow} />
+        <View style={styles.paddingWrap}>
           <View style={globalStyles.row}>
-            <View style={globalStyles.container}>
-              <Text style={styles.text}>Only tracked exercises</Text>
-              <Switch value={onlyShowTracked} onValueChange={(v) => setOnlyShowTracked(v)} />
-            </View>
-            <View style={globalStyles.container}>
-              <Text style={styles.text}>Only bodyweight</Text>
-              <Switch value={onlyBodyweight} onValueChange={(v) => setOnlyBodyweight(v)} />
+            <View style={globalStyles.row}>
+              <View style={globalStyles.container}>
+                <Text style={styles.text}>Only tracked exercises</Text>
+                <Switch value={onlyShowTracked} onValueChange={(v) => setOnlyShowTracked(v)} />
+              </View>
+              <View style={globalStyles.container}>
+                <Text style={styles.text}>Only bodyweight</Text>
+                <Switch value={onlyBodyweight} onValueChange={(v) => setOnlyBodyweight(v)} />
+              </View>
             </View>
           </View>
-        </View>
-        <View style={styles.listContainer}>
-          <View>
-            <TextInput value={exerciseInput} onChangeText={(t) => setExerciseInput(t)} placeholder="Search for exercises" />
+          <View style={styles.listContainer}>
+            <View>
+              <TextInput value={exerciseInput} onChangeText={(t) => setExerciseInput(t)} placeholder="Search for exercises" />
+            </View>
+            <ExerciseSearch refetchParent={refetchParent} input={exerciseInput} username={username} onlyShowTracked={onlyShowTracked} onlyBodyweight={onlyBodyweight} />
           </View>
-          <ExerciseSearch refetchParent={refetchParent} input={exerciseInput} username={username} onlyShowTracked={onlyShowTracked} onlyBodyweight = {onlyBodyweight}/>
         </View>
-      </View>
+      </SafeAreaView>
     </Modal>
-  </SafeAreaView>
   );
 };
 export default ExerciseModal;
