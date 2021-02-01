fb1224d9e584a84b18fd8f078f49d58db48ebe1f
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-01 20:56:49 +0200

    old checkout

--- src/client/components/tabs/user/exercise_modal.tsx
+++ src/client/components/tabs/user/exercise_modal.tsx
@@ -17,8 +17,8 @@ const ExerciseModal: React.FC<{ visible: boolean; setVisible: (b: boolean) => vo
   return (
     <Modal visible={visible} onDismiss={() => setVisible(false)} onRequestClose={() => setVisible(false)} animationType={"slide"}>
       <SafeAreaView style = { styles.safeArea }>
-        <Ionicons onPress={() => setVisible(false)} name="arrow-back-sharp" style={styles.arrow} />
         <View style={styles.paddingWrap}>
+          <Ionicons onPress={() => setVisible(false)} name="arrow-back-sharp" style={styles.arrow} />
           <View style={globalStyles.row}>
             <View style={globalStyles.row}>
               <View style={globalStyles.container}>
