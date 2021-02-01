532a2419e536531df9c2e72c3b13b56cb2e2a693
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-01 15:48:54 +0200

    modal test

--- src/client/authenticator.tsx
+++ src/client/authenticator.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, Suspense } from "react";
 import { gql, useLazyQuery } from "@apollo/client";
-import Loading from "./util_components/loading";
+import ExerciseModal from "./components/tabs/user/exercise_modal";
 const App = React.lazy(() => import("./App"));
 const AppDemo = React.lazy(() => import("./components/app_demo/app_demo"));
 
@@ -11,26 +11,5 @@ const USERNAME = gql`
 `;
 
 export default function Authenticator() {
-  const [googleLoggedIn, setGoogleLoggedIn] = useState(false);
-
-  //try fetch the current user if we have a token (if not logged in google first we need to sign in)
-  const [refetchUser, { data }] = useLazyQuery(USERNAME);
-  useEffect(() => {
-    refetchUser()
-  }, [])
-  if (!data) {
-    return null;
-  }
-  if (!data.username) {
-    return (
-      <Suspense fallback={<Loading />}>
-        <AppDemo refetchUser={refetchUser} googleLoggedIn={googleLoggedIn} setGoogleLoggedIn={setGoogleLoggedIn} />
-      </Suspense>
-    );
-  }
-  return (
-    <Suspense fallback={<Loading />}>
-      <App username={data.username} />
-    </Suspense>
-  );
+  return(<ExerciseModal username = "orek" visible = {true} setVisible = {(arg: boolean) => {}} refetchParent= {() => {}} />)
 }
