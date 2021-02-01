8f8c09d6a7f652d64874719ab8c2462461c9c258
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-01 18:22:10 +0200

    Fixed breaking changes

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import User from "./components/tabs/user/user";
@@ -12,13 +12,13 @@ const App: React.FC<{ username: string }> = ({ username }) => {
   const navigationRef = useRef<any>();
 
   //init the analytics tracker and sender
-  useAnalyticsSender(username);
+  useAnalyticsSender(username)
   return (
     <NavigationContainer
       ref={navigationRef}
       onStateChange={() => {
         const tab = navigationRef.current.getCurrentRoute().name;
-        //set the tab as the visible section
+        //set the tab as the visible section 
         //group is never set as opening group will also open enemy or members. I'm adding "tab" because postgres doesnt like "user" as a variable name
         if (tab !== "Group") visibleSection(tab + "Tab");
       }}
