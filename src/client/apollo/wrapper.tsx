137b945bedfc49d3fb10decc116b70f2596f09e0
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-05 13:13:54 +0200

    await

--- src/client/apollo/wrapper.tsx
+++ src/client/apollo/wrapper.tsx
@@ -1,10 +1,14 @@
 import registerRootComponent from "expo/build/launch/registerRootComponent";
 import React, { useEffect } from "react";
-import { View} from "react-native";
+import { View } from "react-native";
 import { AppleAuthenticationButton, AppleAuthenticationButtonType, AppleAuthenticationButtonStyle, signInAsync, AppleAuthenticationScope, isAvailableAsync } from "expo-apple-authentication";
 const index: React.FC = () => {
   useEffect(() => {
-    isAvailableAsync().then((available) => alert({available: available.valueOf()}))
+    const wait = async () => {
+      const result = await isAvailableAsync();
+      alert(result);
+    };
+    wait()
   }, []);
   return (
     <View style={{ height: "100%", width: "100%", flex: 1, justifyContent: "center", alignItems: "center" }}>
