2133b1055db5775f99fb4805468e4542c70bb562
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-05 13:03:50 +0200

    requestedScopes

--- src/client/apollo/wrapper.tsx
+++ src/client/apollo/wrapper.tsx
@@ -1,13 +1,15 @@
 import registerRootComponent from "expo/build/launch/registerRootComponent";
 import React from "react";
 import { View, Text } from "react-native";
-import { AppleAuthenticationButton, AppleAuthenticationButtonType, AppleAuthenticationButtonStyle, signInAsync } from "expo-apple-authentication";
+import { AppleAuthenticationButton, AppleAuthenticationButtonType, AppleAuthenticationButtonStyle, signInAsync, AppleAuthenticationScope } from "expo-apple-authentication";
 const index: React.FC = () => (
   <View style={{ height: "100%", width: "100%", flex: 1, justifyContent: "center", alignItems: "center" }}>
     <AppleAuthenticationButton
       style={{ width: "70%", height: "10%" }}
       onPress={async () => {
-        await signInAsync()
+        await signInAsync({
+          requestedScopes: [AppleAuthenticationScope.EMAIL]
+        })
           .then((response) => alert(response.identityToken))
           .catch((error) => console.log(error));
       }}
