bead9423a206792aa69a0943c6ba23ae861462a6
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-05 12:52:03 +0200

    signing in

--- src/client/apollo/wrapper.tsx
+++ src/client/apollo/wrapper.tsx
@@ -1,10 +1,18 @@
 import registerRootComponent from "expo/build/launch/registerRootComponent";
 import React from "react";
 import { View, Text } from "react-native";
-import { AppleAuthenticationButton, AppleAuthenticationButtonType, AppleAuthenticationButtonStyle } from "expo-apple-authentication";
+import { AppleAuthenticationButton, AppleAuthenticationButtonType, AppleAuthenticationButtonStyle, signInAsync } from "expo-apple-authentication";
 const index: React.FC = () => (
-  <View style={{  height: "100%", width: "100%", flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <AppleAuthenticationButton style={{ width: "70%", height: '10%' }} onPress={() => alert("wowa")} buttonType={AppleAuthenticationButtonType.SIGN_UP} buttonStyle={AppleAuthenticationButtonStyle.WHITE_OUTLINE} />
-   </View>
+  <View style={{ height: "100%", width: "100%", flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <AppleAuthenticationButton
+      style={{ width: "70%", height: "10%" }}
+      onPress={async () => {
+        const user = await signInAsync();
+        alert(user);
+      }}
+      buttonType={AppleAuthenticationButtonType.SIGN_UP}
+      buttonStyle={AppleAuthenticationButtonStyle.WHITE_OUTLINE}
+    />
+  </View>
 );
 registerRootComponent(index);
