3e15a2288b6b36d3485dd8855fc81d5403962c79
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-05 11:57:21 +0200

    button

--- src/client/apollo/wrapper.tsx
+++ src/client/apollo/wrapper.tsx
@@ -1,7 +1,10 @@
 import registerRootComponent from "expo/build/launch/registerRootComponent";
 import React from "react";
 import {View} from "react-native";
+import {AppleAuthenticationButton, AppleAuthenticationButtonType, AppleAuthenticationButtonStyle} from "expo-apple-authentication";
 const index: React.FC = () => (
-  <View style = {{ backgroundColor: 'blue', height: '100%', width: '100%'}}/>
+  <View style = {{ backgroundColor: 'blue', height: '100%', width: '100%'}}>
+    <AppleAuthenticationButton onPress = {() => alert('wowa')} buttonType = {AppleAuthenticationButtonType.SIGN_UP} buttonStyle = {AppleAuthenticationButtonStyle.WHITE_OUTLINE}/>
+  </View>
 );
 registerRootComponent(index);
