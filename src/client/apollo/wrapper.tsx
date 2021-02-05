0d19b410854094c9da54e900523633d0584cb835
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-05 13:11:00 +0200

    is available

--- src/client/apollo/wrapper.tsx
+++ src/client/apollo/wrapper.tsx
@@ -1,21 +1,26 @@
 import registerRootComponent from "expo/build/launch/registerRootComponent";
-import React from "react";
-import { View, Text } from "react-native";
-import { AppleAuthenticationButton, AppleAuthenticationButtonType, AppleAuthenticationButtonStyle, signInAsync, AppleAuthenticationScope } from "expo-apple-authentication";
-const index: React.FC = () => (
-  <View style={{ height: "100%", width: "100%", flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <AppleAuthenticationButton
-      style={{ width: "70%", height: "10%" }}
-      onPress={async () => {
-        await signInAsync({
-          requestedScopes: [AppleAuthenticationScope.EMAIL]
-        })
-          .then((response) => alert(response.identityToken))
-          .catch((error) => console.log(error));
-      }}
-      buttonType={AppleAuthenticationButtonType.SIGN_UP}
-      buttonStyle={AppleAuthenticationButtonStyle.WHITE_OUTLINE}
-    />
-  </View>
-);
+import React, { useEffect } from "react";
+import { View} from "react-native";
+import { AppleAuthenticationButton, AppleAuthenticationButtonType, AppleAuthenticationButtonStyle, signInAsync, AppleAuthenticationScope, isAvailableAsync } from "expo-apple-authentication";
+const index: React.FC = () => {
+  useEffect(() => {
+    isAvailableAsync().then((available) => alert({available}))
+  }, []);
+  return (
+    <View style={{ height: "100%", width: "100%", flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppleAuthenticationButton
+        style={{ width: "70%", height: "10%" }}
+        onPress={async () => {
+          await signInAsync({
+            requestedScopes: [AppleAuthenticationScope.EMAIL],
+          })
+            .then((response) => alert(response.identityToken))
+            .catch((error) => console.log(error));
+        }}
+        buttonType={AppleAuthenticationButtonType.SIGN_UP}
+        buttonStyle={AppleAuthenticationButtonStyle.WHITE_OUTLINE}
+      />
+    </View>
+  );
+};
 registerRootComponent(index);
