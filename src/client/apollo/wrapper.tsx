f48d975d4799cf35f57fd7ac186d2a1421b29d4c
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-05 12:01:36 +0200

    bb

--- src/client/apollo/wrapper.tsx
+++ src/client/apollo/wrapper.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import { View, Text } from "react-native";
 import { AppleAuthenticationButton, AppleAuthenticationButtonType, AppleAuthenticationButtonStyle } from "expo-apple-authentication";
 const index: React.FC = () => (
-  <View style={{ backgroundColor: "blue", height: "100%", width: "100%", flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <View style={{ width: "100%" }}>
+  <View style={{  height: "100%", width: "100%", flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ width: "100%", height: '20%',  backgroundColor: 'blue'}}>
       <AppleAuthenticationButton style={{ width: "70%" }} onPress={() => alert("wowa")} buttonType={AppleAuthenticationButtonType.SIGN_UP} buttonStyle={AppleAuthenticationButtonStyle.WHITE_OUTLINE} />
     </View>
     <View>
