711eb24613f36ffb160a7bd539432e94282045fe
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-05 12:02:56 +0200

    No wrapper view

--- src/client/apollo/wrapper.tsx
+++ src/client/apollo/wrapper.tsx
@@ -4,9 +4,7 @@ import { View, Text } from "react-native";
 import { AppleAuthenticationButton, AppleAuthenticationButtonType, AppleAuthenticationButtonStyle } from "expo-apple-authentication";
 const index: React.FC = () => (
   <View style={{  height: "100%", width: "100%", flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <View style={{ width: "100%", height: '20%',  backgroundColor: 'blue'}}>
-      <AppleAuthenticationButton style={{ width: "70%", height: '100%' }} onPress={() => alert("wowa")} buttonType={AppleAuthenticationButtonType.SIGN_UP} buttonStyle={AppleAuthenticationButtonStyle.WHITE_OUTLINE} />
-    </View>
+    <AppleAuthenticationButton style={{ width: "70%", height: '10%' }} onPress={() => alert("wowa")} buttonType={AppleAuthenticationButtonType.SIGN_UP} buttonStyle={AppleAuthenticationButtonStyle.WHITE_OUTLINE} />
     <View>
       <Text>hello world</Text>
     </View>
