af1f6e1b5ca1f311ba2f06102a1d4d732da5d379
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-14 21:09:18 +0200

    React app should work now

--- src/backend/src/index.js
+++ src/backend/src/index.js
@@ -33,11 +33,10 @@ const postgraphileOptions = {
 const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
-  //res.send('hello world');
-  res.sendFile(path.join(__dirname,  'public', 'index.html'));
-});
 (async () => {
   app.use(postgraphile(process.env.DATABASE_USER_URL, postgraphileOptions));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,  'public', 'index.html'));
+  });
 })();
 app.listen(process.env.PORT || 4000);
