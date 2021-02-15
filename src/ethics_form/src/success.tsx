2f66060c95e11aa8b7c351d9b14eca5268dadb28
Eerik Saksi <saksi.eerik@gmail.com>
2021-02-15 12:46:01 +0200

    ethics_form into backend

--- src/ethics_form/src/success.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const Success: React.FC = () =>  (
-  <div style={{ height: "100wh", width: "100wv", background: "linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(180,21,227,1) 25%, rgba(134,69,234,1) 54%, rgba(0,212,255,1) 100%)" }} />
-)
-
-export default Success;
