1f3f47c12b2fc07ac1c63bb0d4730de44b97b477
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-08 16:11:26 +0200

    Setting up graph

--- data/user_data.sql
+++ data/user_data.sql
@@ -1,20 +1,9 @@
 --user strength (44%, 2.22 damage)
-
 select "user".username, count(slug_name) as num_exercises, avg(strongerpercentage) as strength,  avg(strongerpercentage) / 100 *  ln(count(slug_name) + 1) * 2.5  as damage
   from "user" inner join "user_exercise" on "user".username = "user_exercise".username
   group by "user".username
   order by avg(strongerpercentage);
 
-select "user".username, count(slug_name) as num_exercises, avg(strongerpercentage) as strength,  avg(strongerpercentage) / 100 *  ln(count(slug_name) + 1) * 2.5  as damage
-  from "user" inner join "user_exercise" on "user".username = "user_exercise".username
-  group by "user".username
-  order by avg(strongerpercentage);
-
---app use (average opens 22)
-select "user".username, count("session_analytics".created_at)
-  from "user" inner join "session_analytics" on "user".username = "session_analytics".username
-  group by "user".username
-  order by count("session_analytics".created_at);
 
 --team performance (average enemy level 4.40
 select "battle".* 
@@ -33,5 +22,19 @@ where "user".username = "workout".username
 and "user".groupName= group_query.groupName
 group by group_query.group_damage, "user".username;
 
+
+--app use (average opens 22)
+select "user".username, count("session_analytics".created_at)
+  from "user" inner join "session_analytics" on "user".username = "session_analytics".username
+  group by "user".username
+  order by count("session_analytics".created_at);
+
+
+--total use (avg 16 minutes) 
+select username, sum(analytics) / 60 from (
+select username, (select sum(time_spent) from unnest(analytics)) as analytics
+  from "session_analytics" 
+  group by session_analytics.analytics, session_analytics.username
+) query group by username order by sum(analytics) ;
 
 
