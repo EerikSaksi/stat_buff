afe4ce40efd7474cdb40de9b5d4c8db95093c0f3
Eerik Saksi <saksi.eerik@gmail.com>
2021-04-15 18:37:24 +0300

    test

--- backend/src/strength_level/wilks.js
+++ backend/src/strength_level/wilks.js
@@ -1,8 +1,9 @@
 const calcRelative = (hardcoded, inputWeight) => {
   const ratio = Math.min((inputWeight - hardcoded.min) / (hardcoded.max - hardcoded.min));
-  const normalizedRatio = ratio / (1 + Math.exp(-(ratio + 2) / 3))
-  console.log(ratio)
+  const normalizedRatio = ratio / (1 + Math.exp(-(ratio + -0.45640529441257865) * 5.9014971670057115))
+  console.log({ratio, normalizedRatio})
 };
 const ratios = [102, 129, 161, 197, 235];
 ratios.forEach((weight) => calcRelative({ min: 102, max: 235 }, weight));
+
 
